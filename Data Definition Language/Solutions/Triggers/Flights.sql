f6757bcc7be4e3834d61ededd9ab2f751140dac5
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2020-05-14 00:19:24 +0300

    Create Flights.sql

---
+++ Data Definition Language/Solutions/Triggers/Flights.sql
@@ -0,0 +1,61 @@
+-- github.com/DaniAngelov
+
+--1. ALTER TABLE FLIGHT ADD num_pass int
+--2. ALTER TABLE AGENCY ADD num_book int
+
+-- 3.
+
+CREATE TRIGGER UPDEPT
+
+ON Booking
+
+AFTER INSERT
+
+AS UPDATE Agency SET Agency.num_book =
+(SELECT COUNT(c.ID) 
+FROM BOOKING b 
+JOIN CUSTOMER c 
+ON b.CUSTOMER_ID = c.ID
+) 
+
+CREATE TRIGGER INSERT_FLIGHT
+
+ON Booking
+
+AFTER INSERT
+
+AS UPDATE Flight SET Flight.num_pass = 
+(SELECT COUNT(c.ID) 
+FROM BOOKING b 
+JOIN CUSTOMER c 
+ON b.CUSTOMER_ID = c.ID
+) 
+
+-- 4. 5.
+
+CREATE TRIGGER UPDEPT
+
+ON Booking
+
+AFTER INSERT
+
+AS UPDATE Agency SET Agency.num_book =
+(SELECT COUNT(c.ID) 
+FROM BOOKING b 
+JOIN CUSTOMER c 
+ON b.CUSTOMER_ID = c.ID
+) 
+
+
+CREATE TRIGGER INSERT_FLIGHT
+
+ON Booking
+
+AFTER DELETE
+
+AS UPDATE Flight SET Flight.num_pass = 
+(SELECT COUNT(c.ID) 
+FROM BOOKING b 
+JOIN CUSTOMER c 
+ON b.CUSTOMER_ID = c.ID
+) 
